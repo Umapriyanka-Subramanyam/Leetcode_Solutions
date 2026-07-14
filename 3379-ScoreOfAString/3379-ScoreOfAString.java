4f57330069cd1d496a2bb5b969da9d21602c3ca5
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:17:37 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 2:17:37 PM
+class Solution {
+    public int scoreOfString(String s) {
+        int ans=0;
+	    for(int i=0;i<s.length()-1;i++){
+	        int first=i;
+	        int last=i+1;
+	        char ch1=s.charAt(first);
+	        char ch2=s.charAt(last);
+	        int ascii1=ch1;
+	        int ascii2=ch2;
+	        int temp=(Math.abs(ascii1-ascii2));
+	        ans=ans+temp;
+	    }
+	    return(ans);
+        
+    }
+}
