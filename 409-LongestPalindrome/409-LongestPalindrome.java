f84c908b885f0df6fd759430f5ebf7fe0c894cf3
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:17 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:18:17 PM
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] arr=new int[128];
+        boolean isOdd = false;
+        int result=0;
+        for(char ch: s.toCharArray()){
+            arr[ch]++;}
+        for(int count : arr){
+            if(count % 2==0)
+            result += count;
+            else{
+                isOdd = true;
+                result+=count-1;
+            } }
+        if(isOdd)result+=1;
+        return result;}}
