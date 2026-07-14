5f7bc888be689cfe9caf5125b08e85562e47cfad
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:15 +0530

    Create 434-NumberOfSegmentsInAString

---
+++ 434-NumberOfSegmentsInAString/434-NumberOfSegmentsInAString.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:18:15 PM
+class Solution {
+    public int countSegments(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+           if(s.charAt(i)!=' ' && (i==0||s.charAt(i-1)==' ')){
+               count++;
+           }
+        }
+        return count;
+
+    }
+}
