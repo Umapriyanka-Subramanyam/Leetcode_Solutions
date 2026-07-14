9a5a37720eafde77a3cc1f76a6c9e6457f5e859c
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:17:40 +0530

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:17:40 PM
+class Solution {
+    public int finalValueAfterOperations(String[] str) {
+        int ans=0;
+        for(int i=0;i<str.length;i++){
+            if(str[i].equals("++X")||str[i].equals("X++")){
+                ans=ans+1;
+            }
+            else{
+                ans=ans-1;
+            }
+        }
+        return ans;
+    }
+}
