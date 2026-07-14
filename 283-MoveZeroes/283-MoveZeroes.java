971e062eb981c0016b547660c0d02eb88fd6536c
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:32 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:18:32 PM
+class Solution {
+    public void moveZeroes(int[] nums) 
+    {      
+          int temp[] =new int[nums.length];
+          int k=0;
+          for(int i=0;i<nums.length;i++)
+          {
+            if(nums[i]!=0)
+            temp[k++]=nums[i];
+          }
+          for(int i=0;i<nums.length;i++)
+          nums[i]=temp[i];
+    
+          System.out.println(Arrays.toString(nums));
+    }   
+}
