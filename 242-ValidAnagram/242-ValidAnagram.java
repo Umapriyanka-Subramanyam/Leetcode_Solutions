7dad53073d96cdb7d251a6818827b9a169182cc0
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:37 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:18:37 PM
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
