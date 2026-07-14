4c63251ba7b3d9591409413f6fab7bcda677b8d9
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:17:53 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:17:52 PM
+class Solution 
+{
+    public String defangIPaddr(String str) 
+    {
+      String ans="";
+      for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        if(ch=='.'){
+            ans=ans+"[.]" ;
+        }
+        else{
+            ans=ans+ch;
+        }
+
+       }
+       return ans;
+    }
+}
