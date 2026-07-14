b4774f8feb5a5e2bf1275f5af2e5d631c1dab216
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:11 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:18:10 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num <= 1) return false;
+        int sum = 1;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                sum += i;
+                if(i != num / i){
+                    sum += num / i;
+                }
+            }
+        }
+        return sum == num;
+    }
+}
