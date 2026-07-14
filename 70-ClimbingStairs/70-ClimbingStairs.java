dcf99e3080dbf97f778968ee9e0d164e67ef4bf4
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:19:32 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:19:32 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1)
+        return 1;
+        int dp[]=new int [n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }     
+        return dp[n];
+    }
+}
