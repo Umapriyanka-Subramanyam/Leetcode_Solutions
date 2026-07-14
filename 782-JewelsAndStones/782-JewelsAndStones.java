c9b3caf03b370cac2b0da6cf50068bb0b2269f5b
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:02 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 2:18:02 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int ans = 0;
+        for(int i=0;i<stones.length();i++){
+            char ch=stones.charAt(i); 
+            boolean found=false;
+            for(int j=0;j<jewels.length();j++){
+             char dia=jewels.charAt(j);
+             if(ch==dia){
+                found=true;
+                break; 
+              }
+            }
+            if(found == true){
+                ans=ans+1;
+            }  
+        }
+       
+        return ans;
+    }
+}
