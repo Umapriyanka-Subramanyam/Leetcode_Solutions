a40348c7e7f62c6df11922927662dc6d4cf39c5b
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:31 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,23 @@
+// Last updated: 7/14/2026, 2:18:31 PM
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+    String arr[]=s.split(" ");
+    if(pattern.length()!=arr.length)
+    return false; 
+    HashMap<Character,String> map= new HashMap<>();
+    for(int i=0;i<pattern.length();i++){
+        char c=pattern.charAt(i);
+        String w=arr[i];
+        if(map.containsKey(c)){
+            if(!map.get(c).equals(w))
+            return false;
+        }
+        else{
+            if(map.containsValue(w))
+            return false;
+            map.put(c,w);
+        }
+    }
+    return true;
+    }
+}
