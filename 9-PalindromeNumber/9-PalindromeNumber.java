160cb73d9ab645fead3402978205156b94f44e7a
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:20:09 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:20:08 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
