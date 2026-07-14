1684b0199a843f54de4aa5b7bfdde061d15ddaf9
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:19:49 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.java
@@ -0,0 +1,20 @@
+// Last updated: 7/14/2026, 2:19:49 PM
+class Solution {
+    public int jump(int[] nums) {
+        int ans=0;
+        int end=0;
+        int far=0;
+        for(int i=0;i<nums.length-1;++i){
+            far=Math.max(far,i+nums[i]);
+            if(far>=nums.length-1){
+                ++ans;
+                break;
+            }
+            if(i==end){
+                ++ans;
+                end=far;
+            }
+        }
+        return ans;
+    }
+}
