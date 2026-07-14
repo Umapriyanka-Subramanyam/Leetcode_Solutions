fe231b7fc7d2f7cb2f517c6782a9bb8726f5b5a4
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:19:05 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:19:05 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<32;++i){
+          int sum=0;
+          for(final int num:nums)
+            sum+=num>>i&1;
+          sum%=3;
+          ans|=sum << i;
+    }
+    return ans;
+  }
+}
