5561f3ede3b802a625f77955bbb4a63f2812c7e0
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:19:43 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,24 @@
+// Last updated: 7/14/2026, 2:19:43 PM
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        
+        double pow = 1;
+        
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        
+        return pow;
+    }
+}
