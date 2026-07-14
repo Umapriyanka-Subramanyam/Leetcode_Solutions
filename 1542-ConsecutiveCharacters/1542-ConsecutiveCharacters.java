799e82d5c4cba20f1c61e20a5249cf8b605fed01
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:17:49 +0530

    Create 1542-ConsecutiveCharacters

---
+++ 1542-ConsecutiveCharacters/1542-ConsecutiveCharacters.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 2:17:49 PM
+class Solution {
+    public int maxPower(String s) {
+        
+        if(s.length()==1)
+            return 1;
+
+        int maximum = Integer.MIN_VALUE;
+        int count = 1;
+
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                count++;
+            }else{
+                count=1;
+            }
+            maximum = Math.max(maximum,count);
+        }
+
+        return maximum;
+    }
+}
