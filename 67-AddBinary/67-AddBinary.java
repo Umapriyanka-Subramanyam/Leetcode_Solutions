d87678a407f637bda95c567178f58f9aeed3a7e5
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:19:36 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 2:19:36 PM
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder s = new StringBuilder();
+        int flag=0;
+        int i=a.length()-1;
+        int j=b.length()-1;
+        while(i>=0||j>=0||flag==1){
+            if(i>=0)
+              flag += a.charAt(i--) -'0';
+            if(j>=0)
+              flag += b.charAt(j--) -'0';
+            s.append(flag%2);
+            flag/=2; 
+        }
+        return s.reverse().toString();
+    }
+}
