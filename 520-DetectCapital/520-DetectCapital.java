084b51d915d5aa8c3649c8a5b742f4abb51d9016
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:18:09 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:18:09 PM
+class Solution {
+    public boolean detectCapitalUse(String word) {
+     char a[]=word.toCharArray();
+     int c=0;
+     for(int i:a)
+     {
+        if(Character.isUpperCase(i))
+        {
+            c++;
+        }    
+     }      
+     return c==a.length || c==0 || (c==1 && Character.isUpperCase(a[0]));
+  }
+}
