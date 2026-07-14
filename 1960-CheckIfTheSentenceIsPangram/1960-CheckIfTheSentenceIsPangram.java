8fac0a931c3999785fe90c6c898faaef169819a5
Umapriyanka-Subramanyam <255828254+Umapriyanka-Subramanyam@users.noreply.github.com>
2026-07-14 14:17:42 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 2:17:42 PM
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> ch=new HashSet<>();
+        for(int i=0; i<sentence.length(); i++){
+         ch.add(sentence.charAt(i));
+        }
+        for(char i='a'; i<='z'; i++){
+            if(!ch.contains(i)) return false;
+        }
+        return true;
+    }
+}
